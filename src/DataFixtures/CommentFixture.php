6c26fdbb44856f1c28d3b435ccf0bf7b70098b2e
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-11-28 17:30:56 +0100

    Awesome random fixtures chapter ( Episode 4 )

--- src/DataFixtures/CommentFixture.php
+++ src/DataFixtures/CommentFixture.php
@@ -4,10 +4,16 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class CommentFixture extends BaseFixture
+class CommentFixture extends BaseFixture implements DependentFixtureInterface
 {
+    public function getDependencies()
+    {
+        return [ArticleFixtures::class];
+    }
+
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(Comment::class, 100, function (Comment $comment){
@@ -19,7 +25,7 @@ class CommentFixture extends BaseFixture
 
             $comment->setAuthorName($this->faker->name);
             $comment->setCreatedAt($this->faker->dateTimeBetween('-1 months', '-1 seconds'));
-            $comment->setArticle($this->getReference(Article::class.'_0'));
+            $comment->setArticle($this->getRandomReference(Article::class));;
         });
 
         $manager->flush();
