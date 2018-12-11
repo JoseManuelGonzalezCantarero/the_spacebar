def312551ec5fc095e77f3c61fe6b862b64c2399
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-12-11 12:58:49 +0100

    Adding a custom voter chapter ( Episode 5 )

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -62,12 +62,10 @@ EOF
 
     /**
      * @Route("/admin/article/{id}/edit")
+     * @IsGranted("MANAGE", subject="article")
      */
     public function edit(Article $article)
     {
-        if (!$this->isGranted('MANAGE', $article)) {
-            throw $this->createAccessDeniedException('No access!');
-        }
         dd($article);
     }
 }
