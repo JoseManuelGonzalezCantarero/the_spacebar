c947b24c1db939feb2b6b07bd1d18665b2a01230
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-12-12 16:30:21 +0100

    Bind your form to a class chapter ( episode 6 )

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Article;
 
 class ArticleFormType extends AbstractType
 {
@@ -13,5 +15,12 @@ class ArticleFormType extends AbstractType
             ->add('title')
             ->add('content')
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class
+        ]);
     }
 }
