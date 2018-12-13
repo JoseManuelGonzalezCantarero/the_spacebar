b940eaac46b24fcb1bfa01473097bb72cdfa6308
Manu Gonzalez <jgonzalezcantarero@gmail.com>
2018-12-13 11:42:46 +0100

    HTML5 & "sanity" validation chapter ( episode 6 )

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -37,6 +37,7 @@ class ArticleFormType extends AbstractType
                 },
                 'placeholder' => 'Choose an author',
                 'choices' => $this->userRepository->findAllEmailAlphabetical(),
+                'invalid_message' => 'Symfony is too smart for your hacking!'
             ])
         ;
     }
