dbdb0614ec77eb2af3d7fb22acb1f59257f41d1a
Manu <jmgonzalez@hola.com>
2018-12-21 10:25:02 +0100

    Hooking up the AJAX autocomplete chapter ( Episode 6 )

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -27,4 +27,14 @@ class UserRepository extends ServiceEntityRepository
             ->execute()
         ;
     }
+
+    public function findAllMatching(string $query, int $limit = 5)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.email LIKE :query')
+            ->setParameter('query', '%'.$query.'%')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+    }
 }
