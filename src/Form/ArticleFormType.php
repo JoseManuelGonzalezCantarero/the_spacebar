8e836952b3fa0d04a255429d1320ac07befc2c3d
Manu <jmgonzalez@hola.com>
2018-12-21 12:36:04 +0100

    Tweak your form based on the underlying data chapter ( Episode 6 )

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -20,6 +20,8 @@ class ArticleFormType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $article = $options['data'] ?? null;
+        $isEdit = $article && $article->getId();
         $builder
             ->add('title', TextType::class, [
                 'help' => 'Choose something catchy!'
@@ -27,17 +29,23 @@ class ArticleFormType extends AbstractType
             ->add('content', null, [
                 'rows' => 15
             ])
-            ->add('publishedAt', null, [
-                'widget' => 'single_text'
+            ->add('author', UserSelectTextType::class, [
+                'disabled' => $isEdit
             ])
-            ->add('author', UserSelectTextType::class)
         ;
+
+        if ($options['include_published_at']) {
+            $builder->add('publishedAt', null, [
+                'widget' => 'single_text'
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Article::class
+            'data_class' => Article::class,
+            'include_published_at' => false
         ]);
     }
 }
